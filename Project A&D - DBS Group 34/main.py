4ab4c39ea75485e1e9184a1675eb6aeee5a46866
JibbeS05 <jibbe.schiettecatte@ugent.be>
2025-11-04 13:32:00 +0100

    Initiële structuur 2

---
+++ Project A&D - DBS Group 34/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        from . import routes  # Import routes to register them with the app
+        db.create_all()  # Create database tables for our data models
+
+    return app
