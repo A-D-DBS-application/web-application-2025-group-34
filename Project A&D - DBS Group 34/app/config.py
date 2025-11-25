3ec51177e4d005a52e3d8f8277eac4ce6e6bf4cb
JibbeS05 <jibbe.schiettecatte@ugent.be>
2025-11-25 14:04:57 +0100

    Connectie met Supabase werkt

--- Project A&D - DBS Group 34/app/config.py
+++ Project A&D - DBS Group 34/app/config.py
@@ -1,5 +1,5 @@
 class Config: 
     SECRET_KEY = 'gr8t3rth4nth3s3cur3k3y'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:4JSSUixyNlY51wWw@db.reexofzxklgbyxkwaonu.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.reexofzxklgbyxkwaonu:4JSSUixyNlY51wWw@aws-1-eu-north-1.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
